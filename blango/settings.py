f339dac5706fd3d3aaab586e7275d9266a0e698a
Oscar Lundberg <lundberg.oscar@gmail.com>
2022-01-19 13:20:24 +0000

    Finish html frameworks

--- blango/settings.py
+++ blango/settings.py
@@ -60,7 +60,7 @@ SESSION_COOKIE_SAMESITE = 'None'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
