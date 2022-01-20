49dd5cf5ebf798c91ae01c52abbb3403b1bcaae7
Oscar Lundberg <lundberg.oscar@gmail.com>
2022-01-20 10:40:29 +0000

    Finish crispy forms

--- blango/settings.py
+++ blango/settings.py
@@ -34,6 +34,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    "crispy_bootstrap5",
     "blog",
 ]
 ROOT_URLCONF = 'blango.urls'
@@ -56,6 +58,8 @@ CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SAMESITE = 'None'
 SESSION_COOKIE_SAMESITE = 'None'
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 TEMPLATES = [
     {
